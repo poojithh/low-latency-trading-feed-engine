4c3984618ba82f4dac4f57ce3afed9e77069d75c
saipoojith <gsaipoojith@gmail.com>
2026-05-28 16:47:32 +0530

    Added JSON configuration system

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "===================================" << std::endl;
+    std::cout << " LOW LATENCY TRADING ENGINE " << std::endl;
+    std::cout << "===================================" << std::endl;
+
+    std::cout << "Engine Started Successfully" << std::endl;
+
+    return 0;
+}
